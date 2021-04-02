ef2229d2a5871cb797b75c982c69f426f14bc39b
anon-ds <rhythm.kumar2@gmail.com>
2021-04-02 23:47:10 +0530

    Update Test.java
    Hello from git

--- Maven-Git-Dummy/src/main/java/com/lti/main/Test.java
+++ Maven-Git-Dummy/src/main/java/com/lti/main/Test.java
@@ -3,6 +3,6 @@ package com.lti.main;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hi there!-->eclipse");
-		
+		System.out.println("Hi there!-->github");
 	}
 }
